9440f531194c528651a07c41690943c90bc5622d
jishaWCUCS <143959518+jishaWCUCS@users.noreply.github.com>
2024-06-18 19:29:01 -0400

    Update setup_ansible.sh

--- setup_ansible.sh
+++ setup_ansible.sh
@@ -6,4 +6,4 @@ cd ansible-playbooks/lamp_ubuntu1804/
 cp /local/repository/default.yml vars/
 HOST=$(hostname -f)
 sed -i "s/HOSTNAME/$HOST/g" vars/default.yml
-ansible-playbook playbook.yml -l server1 -u lngo
+ansible-playbook playbook.yml -l server1 -u Jisha
