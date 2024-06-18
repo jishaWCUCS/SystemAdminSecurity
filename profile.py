50ed02f760bed0b3d2d5bae436c56e63cd88e8f6
jishaWCUCS <143959518+jishaWCUCS@users.noreply.github.com>
2024-06-18 18:50:12 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,16 +1,30 @@
 import geni.portal as portal
 import geni.rspec.pg as rspec
-
+         
 # Create a Request object to start building the RSpec.
 request = portal.context.makeRequestRSpec()
+
+prefixForIP = "192.168.1."
+link = request.LAN("lan")
+
 # Create a XenVM
-node = request.XenVM("node")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
-node.routable_control_ip = "true"
-
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y apache2"))
-node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status apache2'))
-
+for i in range(2):
+  if i == 0:
+    node = request.XenVM("control")
+  else: 
+    node = request.XenVM("host")
+    
+  node.routable_control_ip = "true"
+  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
+  iface = node.addInterface("if" + str(i))
+  iface.component_id = "eth1"
+  iface.addAddress(rspec.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+  link.addInterface(iface)
+  
+  if i == 0:
+    node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/setup_control.sh"))
+  else:
+    node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/setup_host.sh"))
+    
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
