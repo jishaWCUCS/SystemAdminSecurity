01d693eb6a4403fbfe08de6ce89cea2ca899ddf6
jishaWCUCS <143959518+jishaWCUCS@users.noreply.github.com>
2024-06-27 21:07:28 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,16 +1,34 @@
+"""CloudLab Profile for constructing a profile with 2 nodes.
+Assignment 2 profile
+
+Instructions:
+Wait for the profile instance to start, and then log in to the host via the
+ssh port specified below.
+"""
+
+# Import the necessary libraries
 import geni.portal as portal
-import geni.rspec.pg as rspec
+import geni.rspec.pg as pg
 
-# Create a Request object to start building the RSpec.
+# Create a request object to start building the RSpec
 request = portal.context.makeRequestRSpec()
-# Create a XenVM
-node = request.XenVM("node")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
-node.routable_control_ip = "true"
 
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y apache2"))
-node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status apache2'))
+# Define the webserver node
+webserver = request.RawPC("webserver")
+webserver.hardware_type = "m510"
+webserver.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
+webserver.addService(pg.Execute(shell="sh", command="sudo apt-get update && sudo apt-get install -y apache2"))
+webserver.routable_control_ip = True
+
+# Define the observer node
+observer = request.RawPC("observer")
+observer.hardware_type = "m510"
+observer.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
+observer.addService(pg.Execute(shell="sh", command="sudo apt-get update && sudo apt-get install -y nfs-kernel-server"))
+
+# Link the nodes together
+link = request.Link(members=[webserver, observer])
+link.bandwidth = 1000000  # 1 Gbps
 
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
